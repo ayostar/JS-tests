35355eb008583340f28ec2105511d02f6fe919aa
Artem Starodubtsev <ayostar@gmail.com>
2022-09-16 20:03:21 +0300

    test matchers

--- matchers/src/js/app.js
+++ matchers/src/js/app.js
@@ -1,9 +1,44 @@
 import sortHeroHealth from './hero';
 
-console.log(
-  sortHeroHealth([
-    { name: 'мечник', health: 10 },
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-  ])
-);
+const heroName = document.querySelector('#hero');
+
+function showHealth(data) {
+  if (data.health) {
+    const { health } = data;
+    if (health > 50) {
+      return 'healthy';
+    }
+    if (health <= 50 && health >= 15) {
+      return 'wounded';
+    }
+    if (health < 15 && health >= 0) {
+      return 'critical';
+    }
+    return null;
+  }
+  return null;
+}
+
+function showHeroes(array) {
+  array.forEach((element) => {
+    const healthStatus = showHealth(element);
+
+    heroName.insertAdjacentHTML('beforeend', `<div> ${element.name} </div>`);
+    heroName.insertAdjacentHTML(
+      'beforeend',
+      `<div id="health"> <div class="${healthStatus}" style="width: ${element.health}%">${element.health}</div></div>`
+    );
+  });
+}
+
+const heroesArray = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
+
+const sortedHeroes = sortHeroHealth(heroesArray);
+
+console.log(sortHeroHealth(heroesArray));
+
+showHeroes(sortedHeroes);
--- matchers/src/js/hero.js
+++ matchers/src/js/hero.js
@@ -1,4 +1,5 @@
 export default function sortHeroHealth(data) {
-  data.sort((a, b) => (a.health < b.health ? 1 : -1));
+  // data.sort((a, b) => (a.health < b.health ? 1 : -1));
+  data.sort((prev, next) => next.health - prev.health);
   return data;
 }
